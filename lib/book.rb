65c66b7b24a5ccf7e5247404e32ec972967a9877
Anthony Mai <anthony.mai530@gmail.com>
2019-11-24 01:53:50 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,15 +1,12 @@
 class Book
   attr_accessor :author, :page_count
-  attr_reader :title
+  attr_reader :title :genre
   GENRES = []
   def initialize(title)
     @title = title
   end
   def genre=(genre)
     @genre = genre
-  end
-  def genre
-    @genre
   end
   def turn_page
     puts "Flipping the page...wow, you read fast!"
